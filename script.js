94bb98330abfcd735907e28984d5627c23fffbcf
Aram-Gumrikyan <aram.gumrikyan.01@gmail.com>
2020-07-13 23:35:33 +0400

    a

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-let eleman = document.getElementById('happyBrdey');
-// eleman.autoplay = true;
+document.addEventListener("mouseover", function() {
+    let eleman = document.getElementById('happyBrdey');
+    eleman.autoplay = true;
+});
+// let width = window.innerWidth;
+// console.log(width);
+// let ballonWidth = (6 * width) / 100;
+// console.log(ballonWidth);
+// let ballonCount = parseInt(width / ballonWidth);
+// console.log(ballonCount);
+// let ballonGrup = document.querySelector('.ballons');
+// let ballon = document.querySelector('.ballon');
+// console.log(ballonGrup);
+// console.log(ballon);
+
+
+// for (let i = 0; i < ballonCount; i++) {
+// 	ballonGrup.appendChild(ballon);
+// }
+
+// Իմ ամենաթանկ, քույրիկ ջա՜ն, ծնունդդ շնորհավոր, քեզ ամենալավն ու բարին եմ ցանկանում, 
+// թող ժպիտը դեմքիցդ երբեք չպակասի ու աչքերդ միշտ փաիլեն ուրախությունից, ես քեզ շատ-շատ սիրում եմ:
